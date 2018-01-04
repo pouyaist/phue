d96a4c40e85e9fc5667c0db97a72b43e006f5a1e
Pouya Samadi <pspouya90@gmail.com>
2018-01-04 16:26:09 +0100

    add button to turn off with schedule

--- examples/hue_toggle_random_colors.py
+++ examples/hue_toggle_random_colors.py
@@ -8,6 +8,14 @@ def set_light_xy(obj):
     obj.xy = [random.random(),random.random()]
     
 
+
+def schedule_bed_time(check_lit, light_id):   
+    current_time = datetime.now()
+    data = {'on': check_lit, 'transitiontime' : 30}
+    bed_time = current_time + timedelta(seconds=30)
+    bed_time = current_time.strftime('%Y-%m-%dT%H:%M:%S')
+    b.create_schedule('Bed schedule' + str(light_id) + str(random.randint(1, 1000)), bed_time, light_id, data, 'Bedtime')
+
 b = Bridge('192.168.1.10')
 
 root = Tk()
@@ -29,7 +37,7 @@ if not b.get_light(3, 'on'):
     b.set_light(3, 'bri', 150)
 
 
-current_time = datetime.now()
+
 
 
 for light in b.lights:
@@ -46,22 +54,20 @@ for light in b.lights:
     button_var = BooleanVar()
     button_var.set(b.get_light(light_id, 'on'))
     button_command = lambda button_var=button_var, light_id=light_id: b.set_light(light_id, 'on', button_var.get())
-    button = Checkbutton(channel_frame, variable = button_var, command = button_command)
+    button = Checkbutton(channel_frame, variable = button_var, command = button_command, text = "light")
     button.pack()
     
     button_random_command = lambda  light = light: set_light_xy(light)
-    button_random = Button(channel_frame, command = button_random_command)
+    button_random = Button(channel_frame, command = button_random_command, text="Random")
     button_random.pack()
     
+    button_schedule_command = lambda  check_lit = not button_var.get(), light_id = light_id: schedule_bed_time(check_lit, light_id)
+    button_random = Button(channel_frame, command = button_schedule_command, text="Schedule")
+    button_random.pack()
+
     label = Label(channel_frame)
     label.config(text = b.get_light(light_id,'name'))
     label.pack()
-
-    data = {'on': False, 'transitiontime' : 300}
-    bed_time = current_time + timedelta(seconds=30)
-    bed_time = current_time.strftime('%Y-%m-%dT%H:%M:%S')
-    b.create_schedule('Bed schedule' + str(light_id), bed_time, light_id, data, 'Bedtime')
-
 
 
 root.mainloop()
